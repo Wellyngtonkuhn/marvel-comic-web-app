44c65556d6528b1a5fb0f78fbacdd06a8cbe20f5
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2023-04-27 15:27:19 -0300

    Contiuando com o modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,38 +1,84 @@
-
-import { Dispatch, SetStateAction } from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { ModalContent, ModalHeader, ModalSection } from "./style"
-import { faX } from "@fortawesome/free-solid-svg-icons"
+import { Dispatch, SetStateAction } from "react";
+import { useQuery } from "react-query";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ModalContent, ModalHeader, ModalSection } from "./style";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { api } from "../../axios/api";
 
 type ModalData = {
-  id: number
-  setOpenModal: Dispatch<SetStateAction<boolean>>
-}
+  id: number;
+  setOpenModal: Dispatch<SetStateAction<boolean>>;
+};
 
-  export default function Modal({ id, setOpenModal }: ModalData) {
+type DetailsData = [{
+    title: string;
+    description: string;
+    thumbnail: {
+      extension: string;
+      path: string;
+    };
+    images: [{ path: string; extension: string }];
+    creators: {
+      items: [{ name: string; role: string }];
+    };
+    prices: [{ price: number }];
+  }
+];
 
-    const handleCloseModal = () => {
-      setOpenModal(false)
+export default function Modal({ id, setOpenModal }: ModalData) {
+  const { data, isLoading } = useQuery<DetailsData | null>(["comic-detail", id], async () => {
+      const response = await api.get(`comics/${id}`);
+      return response.data.data.results;
+    },
+    {
+      staleTime:((1000 * 60) * 60) * 24, // O resultado fica armazenado em cache por 24h, segundo a docs da api é um bom tempo com base que os dados não mudam muito
     }
+  );
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleFirstLetterToUpperCase = (word: string) : string => {
+    const firstLetter = word.slice(0, 1)
+    const restOfTheWord = word.slice(1, word.length)
+    return firstLetter.toUpperCase() + restOfTheWord
+  }
 
   return (
     <ModalSection>
       <ModalHeader>
-        <h2>Title</h2>
+        <h3>{data?.[0].title}</h3>
         <button onClick={() => handleCloseModal()}>
           <FontAwesomeIcon icon={faX} />
         </button>
       </ModalHeader>
-
       <ModalContent>
         <div>
-        {id}
+          <img
+            src={`${data?.[0].images[0].path}/portrait_uncanny.${data?.[0].images[0].extension}`}
+            alt={data?.[0].title}
+          />
         </div>
-
         <div>
-
+          <h3>
+            Published Arrumar: <span>October 05, 2022</span>
+          </h3>
+          {data &&
+            data[0].creators.items.map((item) => (
+              <h3 key={item.name}>
+                {handleFirstLetterToUpperCase(item.role)}: <span>{item.name}</span>
+              </h3>
+            ))}
+          {data?.[0].prices[0].price != 0 && (
+            <h3>
+              Price: <span>$ {data?.[0].prices[0].price}</span>
+            </h3>
+          )}
+          <p>{data?.[0].description}</p>
+          <button>Enviar</button>
         </div>
       </ModalContent>
     </ModalSection>
-  )
+  );
 }
--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -33,4 +33,111 @@ export const ModalHeader = styled.div`
   }
 `;
 
-export const ModalContent = styled.div``;
+export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  div:nth-child(1) {
+    width: 100%;
+    height: 40%;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  div:nth-child(2) {
+    width: 95%;
+    height: 45%;
+    margin: 0 auto;
+    overflow-y: auto;
+
+    h3 {
+      font-size: 1rem;
+      margin: 0;
+      padding: 0.2rem 0.6rem;
+
+      span {
+        font-size: 0.9rem;
+        font-weight: 400;
+      }
+    }
+
+    p {
+      font-size: 0.8rem;
+      padding: 0.1rem 1rem;
+      margin: 0;
+      text-align: justify;
+    }
+
+    button {
+      width: 100%;
+      background-color: #9f0013;
+      color: #fff;
+      padding: 0.7rem 0;
+      margin: 1rem 0;
+
+      border-radius: 1rem;
+      border: none;
+      font-size: 1rem;
+      font-weight: 700;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    height: calc(100% - 60px);
+
+    div:nth-child(1) {
+      width: 45%;
+      height: 100%;
+      border-bottom-left-radius: 1rem;
+
+      img {
+        height: 96.8%;
+        border-bottom-left-radius: 1rem;
+
+        @media screen and (min-width: 900px) {
+          height: 100%;
+        }
+      }
+    }
+
+    div:nth-child(2) {
+      width: 45%;
+      height: 100%;
+      border-bottom-right-radius: 1rem;
+
+      h3 {
+        font-size: 1.45rem;
+        padding: 0.3rem 0.6rem;
+
+        span {
+          font-size: 1.1rem;
+        }
+      }
+
+      p {
+        font-size: 1rem;
+        padding: 0.1rem 1rem;
+        text-align: justify;
+      }
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    div:nth-child(2) {
+      button {
+        opacity: 0.8;
+        transition: all ease 0.3s;
+        :hover {
+          cursor: pointer;
+          opacity: 1;
+        }
+      }
+    }
+  }
+`;
